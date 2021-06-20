7962f8955242f1c5f70efecff90b7017aac7db24
Claudio Lespier <61632584+datasurf@users.noreply.github.com>
2021-06-20 19:53:06 +0000

    Updated Part 2 of the graded assignment.

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,39 +8,51 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
+let candidateAnswers = [];
 
-function askForName() {
+function askForName(){
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("Please enter your name: ");
+  console.log("");
 }
 
-function askQuestion() {
+function askQuestion(){
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  let answer = "";
+  for (i = 0; i < questions.length; i++){
+    answer = input.question(questions[i]);
+    candidateAnswers.push(answer);
+    console.log("");
+  } 
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  let grade = "";
-  if (candidateAnswer === correctAnswer) {
-    grade = "That is correct. Great job!"
-    console.log(grade);
-  } else {
-    grade = "Sorry that is incorrect, you will never be an astronaut."
-    console.log(grade);
-  } 
-  
-  return grade;
+
+  for (i = 0;i < candidateAnswers.length;i++) {
+    if (candidateAnswers[i] === correctAnswers[i]){
+      console.log(`
+      Your Answer: ${candidateAnswers[i]}  
+      Correct Answer: ${correctAnswers[i]}
+      That is correct! Good job!
+      `);
+    } else {
+       console.log(`
+      Your Answer: ${candidateAnswers[i]}  
+      Correct Answer: ${correctAnswers[i]}
+      Sorry that was incorrect.
+      `);
+    }
+  }
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   console.log("Hello, " + candidateName + " welcome to the astronaut training program!");
+  console.log("");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
